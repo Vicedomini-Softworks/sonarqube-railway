b768959216eceaddc6f35a589bafea80d2fa87e8
sofiavicedomini <sofia@vicedomini.ltd>
2026-03-28 16:46:19 +0100

    Add entrypoint script for SonarQube setup

---
+++ sonar/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Crea directory se non esistono
+mkdir -p "$SONAR_PATH_DATA" \
+         "$SONAR_PATH_EXTENSIONS" \
+         "$SONAR_PATH_LOGS" \
+         "$SONAR_PATH_TEMP"
+
+# Fix permessi (uid 1000 = utente sonarqube)
+chown -R 1000:1000 /opt/mount
+
+# Torna all'utente sonarqube
+exec gosu sonarqube /opt/sonarqube/docker/entrypoint.sh "$@"
